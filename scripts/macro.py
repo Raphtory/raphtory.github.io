c99a4399b9a2589506139638cd7f25513cfe82e4
miratepuffin <b.a.steer@qmul.ac.uk>
2023-09-01 17:41:44 +0100

    Fixed scripts

--- scripts/macro.py
+++ scripts/macro.py
@@ -13,18 +13,18 @@ LANGUAGES = OrderedDict(
         "icon_name": "python",
         "code_name": "python",
     },
-    #rust={
+    # rust={
     #    "extension": ".rs",
     #    "display_name": "Rust",
     #    "icon_name": "rust",
     #    "code_name": "rust",
-    #},
-    #node={
+    # },
+    # node={
     #    "extension": ".js",
     #    "icon_name": "node-js",
     #    "display_name": "NodeJS",
     #    "code_name": "javascript",
-    #},
+    # },
 )
 
 # Load all links to reference docs
