d41029157c201657fdfaec46710f7e4e294eb871
narnolddd <n.a.arnold@qmul.ac.uk>
2024-09-06 17:43:55 +0100

    layers

--- docs/src/python/getting-started/persistent-graph.py
+++ docs/src/python/getting-started/persistent-graph.py
@@ -19,6 +19,14 @@ print(f"G's edges are {G.edges}")
 print(f"G's exploded edges are {G.edges.explode()}")
 # --8<-- [end:simple_graph]
 
+# --8<-- [start:hanging_deletions]
+G = PersistentGraph()
+
+G.delete_edge(5, "Alice", "Bob")
+
+print(G.edges.explode())
+# --8<-- [end:hanging_deletions]
+
 # --8<-- [start:behaviour_1]
 G = PersistentGraph()
 
@@ -45,3 +53,14 @@ print(f"G1's edges are {G1.edges.explode()}")
 print(f"G2's edges are {G2.edges.explode()}")
 
 # --8<-- [end:behaviour_2]
+
+# --8<-- [start:behaviour_3]
+G = PersistentGraph()
+
+G.add_edge(1, "Alice", "Bob", layer="colleagues")
+G.delete_edge(5, "Alice", "Bob", layer="colleagues")
+G.add_edge(3, "Alice", "Bob", layer ="friends")
+G.delete_edge(7, "Alice", "Bob", layer="friends")
+
+print(G.edges.explode())
+# --8<-- [end:behaviour_3]
